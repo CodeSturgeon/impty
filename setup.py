bea8f6e6e1a46f2ce5427c22c80cf3d51f50705b
Nick Fisher <fish@FishBook.local>
2009-10-10 18:28:42 -0400

    Module skel

---
+++ setup.py
@@ -0,0 +1,16 @@
+#from ez_setup import use_setuptools
+#use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="IMAP4PowerToy",
+      version="0.2dev",
+      description="IMAP4 Power Toy",
+      author="Nick Fisher",
+      packages = find_packages(),
+      zip_safe = True,
+      entry_points = {
+          'console_scripts': [
+              'impty = couchnote.tool:main',
+          ]
+      }
+     )
